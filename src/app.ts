be0a1be4d8ea8e43cc421bffac857b7e63165313
ZzzandyzzZ <andy_gean@hotmail.com>
2023-06-08 19:28:48 -0500

    Add apirouter to express app

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,33 @@
 import express from "express";
-import { Client } from "pg";
+
+// import { Client } from "pg";
+
+import { DATABASE_URL, PORT } from "./env_vars";
+import apiRouter from "./modules/shared/api.router";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+app.get("/", (_req, res) => {
+  console.log(DATABASE_URL);
   res.send("Hello, world!");
 });
-const connectionString = process.env.DATABASE_URL;
-const client = new Client({
-  connectionString,
-});
 
-client.connect();
-client.query("SELECT * FROM users", (err, res) => {
-  console.log("error", err);
-  console.log("rpta", res.rows);
-  client.end();
-});
+// const connectionString = DATABASE_URL;
+
+// const client = new Client({
+//   connectionString,
+// });
+
+// client.connect();
+// client.query("SELECT * FROM users", (err, res) => {
+//   console.log("error", err);
+//   console.log("rpta", res.rows);
+//   client.end();
+// });
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
