95f32ca8c6f8e842127b68edebac9c548b150bb2
ZzzandyzzZ <andy_gean@hotmail.com>
2023-06-08 22:20:48 -0500

    Separate express app from main script

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,13 @@
 import express from "express";
 
-// import { Client } from "pg";
+import apiRouter from "./api.router";
 
-import { DATABASE_URL, PORT } from "./env_vars";
-import apiRouter from "./modules/shared/api.router";
-
-const app = express();
+export const app = express();
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
 
 app.get("/", (_req, res) => {
-  console.log(DATABASE_URL);
   res.send("Hello, world!");
-});
-
-// const connectionString = DATABASE_URL;
-
-// const client = new Client({
-//   connectionString,
-// });
-
-// client.connect();
-// client.query("SELECT * FROM users", (err, res) => {
-//   console.log("error", err);
-//   console.log("rpta", res.rows);
-//   client.end();
-// });
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
 });
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { app } from "./app";
+import { AppDataSource } from "./db";
+import { PORT } from "./env_vars";
+
+const main = async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log("Data Source has been initialized!");
+    await app.listen(PORT);
+    console.log(`Server listening on port ${PORT}`);
+  } catch (error) {
+    console.error("Error during app initialization", error);
+  }
+};
+
+main();
