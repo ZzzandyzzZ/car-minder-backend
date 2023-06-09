7e9e4beb56046abfab3115361dda4e083e3dbf86
ZzzandyzzZ <andy_gean@hotmail.com>
2023-06-08 19:28:17 -0500

    Create api router

---
+++ src/modules/shared/api.router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { userRouter } from "../user/dependencies";
+
+const apiRouter = Router();
+
+apiRouter.use("/users", userRouter);
+
+export default apiRouter;
