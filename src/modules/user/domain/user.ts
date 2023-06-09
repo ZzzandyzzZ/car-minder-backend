da1f9265525c2df4721dafa41eb0ae3a353542c3
ZzzandyzzZ <andy_gean@hotmail.com>
2023-06-09 17:39:28 -0500

    Use user interface instead class

--- src/modules/user/domain/user.ts
+++ src/modules/user/domain/user.ts
@@ -1,18 +1,14 @@
-class User {
-  constructor(
-    readonly id: number,
-    readonly createdAt: Date,
-    readonly dni: string,
-    readonly email: string,
-    readonly firstname: string,
-    readonly lastname: string,
-    readonly middlename: string,
-    readonly phone: string,
-    readonly secondLastname: string,
-    readonly token: string
-  ) {
-    this.firstname = firstname;
-  }
+export interface User {
+  id: number;
+  createdAt: Date;
+  dni: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  middlename: string;
+  phone: string;
+  secondLastname: string;
+  token: string;
 }
 
-export default User;
+export type UserShow = Pick<User, "id" | "firstname" | "dni">;
