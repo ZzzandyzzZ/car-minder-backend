b22da73f7a2fa842a9d75d4198ac7ed99b7eca15
ZzzandyzzZ <andy_gean@hotmail.com>
2023-06-25 13:00:45 -0500

    Add naming strategy and paths to entities and migrations

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,15 +1,22 @@
-import { join } from "path";
+// import { join } from "path";
 
 import { DataSource } from "typeorm";
 
 import { DATABASE_URL, NODE_ENV } from "../env-vars";
+import { CarBrandEntity, CarColorEntity, CarEntity, CarModelEntity } from "@car/infraestructure/db";
+import { SnakeNamingStrategy } from "./snake-naming-strategy";
+import { InsertCarBrandData1687437613179 } from "./migrations/1687437613179-InsertCarBrandData";
 
 export const AppDataSource = new DataSource({
   url: DATABASE_URL,
   type: "postgres",
   logging: false,
-  entities: [join(__dirname, "../**", "*-entity.{ts,js}")],
+  entities: [CarEntity, CarBrandEntity, CarColorEntity, CarModelEntity],
+  // entities: [join(__dirname, "../**", "*-entity.{ts,js}")],
   synchronize: NODE_ENV !== "PRD",
-  migrations: [join(__dirname, "migrations", "*.{ts,js}")],
+  migrations: [InsertCarBrandData1687437613179],
+  // migrations: [join(__dirname, "migrations", "*.{ts,js}")],
+  // migrationsRun: true,
   // dropSchema: true,
+  namingStrategy: new SnakeNamingStrategy(),
 });
