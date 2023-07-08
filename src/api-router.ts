d6fa3dc8f0376752f1789dd6914a1ce54811aafd
ZzzandyzzZ <andy_gean@hotmail.com>
2023-07-08 15:46:37 -0500

    Add carRouter

--- src/api-router.ts
+++ src/api-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { userRouter } from "@user/dependencies";
+import { carRouter } from "@car/dependencies";
 
 const apiRouter = Router();
 
+apiRouter.use("/cars", carRouter);
 apiRouter.use("/users", userRouter);
 
 export default apiRouter;
