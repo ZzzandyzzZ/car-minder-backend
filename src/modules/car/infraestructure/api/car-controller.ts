80a3bd1a50b0ab25b7b76f1b4bebd3b15f838b72
ZzzandyzzZ <andy_gean@hotmail.com>
2023-07-08 15:45:48 -0500

    create car controller

--- src/modules/car/infraestructure/api/car-controller.ts
+++ src/modules/car/infraestructure/api/car-controller.ts
@@ -1,12 +1,16 @@
 import { CarService } from "@car/application/car-service";
+import { Request, Response } from "express";
 
 export class CarController {
   private carService: CarService;
   constructor(carService: CarService) {
     this.carService = carService;
   }
-  async getCars(): Promise<Car[]> {
-    const cars = this.carService.getCars();
+  async getCars(_req: Request, res: Response): Promise<Car[]> {
+    const cars = await this.carService.getCars();
+    res.json({
+      cars,
+    });
     return cars;
   }
 }
