b454e08de6ba795d55d76d6b54375f232473fbed
ZzzandyzzZ <andy_gean@hotmail.com>
2023-07-09 18:20:00 -0500

    Create getCarBy controller

--- src/modules/car/infraestructure/api/car-controller.ts
+++ src/modules/car/infraestructure/api/car-controller.ts
@@ -6,11 +6,22 @@ export class CarController {
   constructor(carService: CarService) {
     this.carService = carService;
   }
-  async getCars(_req: Request, res: Response): Promise<Car[]> {
+  async getCars(_req: Request, res: Response): Promise<void> {
     const cars = await this.carService.getCars();
     res.json({
       cars,
     });
-    return cars;
+  }
+  async getCarBy(req: Request, res: Response): Promise<void> {
+    const car = await this.carService.getByLP(req.params.licensePlate);
+    if (!car) {
+      res.status(404).json({
+        message: "Car not found",
+      });
+      return;
+    }
+    res.json({
+      car,
+    });
   }
 }
