c6f4b7868bbb8bcaa81eac21bfeb92b2343d7e51
ZzzandyzzZ <andy_gean@hotmail.com>
2023-07-14 20:17:32 -0500

    Create CarBrandController

---
+++ src/modules/car/related/car-brand/infraestructure/api/car-brand-controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { CarBrandService } from "../../application/car-brand-service";
+
+export class CarBrandController {
+  private service: CarBrandService;
+  constructor(service: CarBrandService) {
+    this.service = service;
+  }
+  async getAll(_req: Request, res: Response) {
+    const brands = await this.service.getAll();
+    res.json({
+      data: brands,
+    });
+  }
+  async getById(req: Request, res: Response) {
+    const brand = await this.service.getById(req.params.id);
+    res.json({
+      data: brand,
+    });
+  }
+}
