f65d398420116ba64af737ed7174ba643764abc2
ZzzandyzzZ <andy_gean@hotmail.com>
2023-07-15 17:07:04 -0500

    Add needed methos in repositories

--- src/modules/car/related/car-brand/domain/car-brand-repository.d.ts
+++ src/modules/car/related/car-brand/domain/car-brand-repository.d.ts
@@ -1,4 +1,1 @@
-interface CarBrandRepository extends BaseRepository<CarBrand> {
-  getAll(): Promise<Array<CarBrand> | []>;
-  getById(id: UUID): Promise<CarBrand | null>;
-}
+interface CarBrandRepository extends BaseRepository<CarBrand> {}
--- src/modules/shared/domain/base-repository.d.ts
+++ src/modules/shared/domain/base-repository.d.ts
@@ -1,6 +1,6 @@
 interface BaseRepository<T> {
-  create?(model: T): Promise<T>;
-  delete?(id: UUID): Promise<number>;
+  create(model: T): Promise<T>;
+  delete(id: UUID): Promise<number>;
   getAll(): Promise<Array<T> | []>;
   getById(id: UUID): Promise<T | null>;
   update?(model: T): Promise<T>;
